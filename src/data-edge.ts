6413b940e2a245d6c6876b9515d1cf782323bfa5
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2022-10-05 19:58:01 -0300

    added stringify method for json, and fixes

--- src/data-edge.ts
+++ src/data-edge.ts
@@ -4,7 +4,9 @@ import {
   json,
   ipfs,
   JSONValue,
-  JSONValueKind
+  JSONValueKind,
+  log,
+  BigDecimal
 } from "@graphprotocol/graph-ts";
 import { DataEdge, SubmitQoSPayloadCall } from "../generated/DataEdge/DataEdge";
 import {
@@ -42,7 +44,7 @@ function processPayload(payload: Bytes, messageID: String): void {
         if (shouldProcessTopic(topic)) {
           let hash = jsonToString(data.get("hash"));
           let timestamp = jsonToBigInt(data.get("timestamp"));
-          processIpfsHash(hash, timestamp, messageID);
+          processIpfsHash(hash, timestamp, messageID.concat(index.toString()));
         }
       }
     } else if (jsonData.value.kind == JSONValueKind.OBJECT) {
@@ -61,27 +63,48 @@ function shouldProcessTopic(topic: String): boolean {
   return topic != "" && JSON_TOPICS.includes(topic);
 }
 
-export function processIpfsHash(ipfsHash: String, timestamp: BigInt, messageID: String): void {
+export function processIpfsHash(
+  ipfsHash: String,
+  timestamp: BigInt,
+  messageID: String
+): void {
   let ipfsData = ipfs.cat(ipfsHash);
-  // let jsonIpfsData = json.try_fromBytes(
-  //   ipfsData ? ipfsData! : Bytes.empty()
-  // );
+  let jsonIpfsData = json.try_fromBytes(ipfsData ? ipfsData! : Bytes.empty());
 
+  let indexerDataPointCount = BIGINT_ZERO;
   let messageDataPoint = new MessageDataPoint(messageID);
   // messageDataPoint.rawData = jsonIpfsData.isOk
   //   ? jsonToString(jsonIpfsData.value)
   //   : "";
   messageDataPoint.rawData = ipfsData ? ipfsData!.toString() : "";
   messageDataPoint.ipfsHash = ipfsHash;
-  messageDataPoint.indexerDataPointCount = BIGINT_ZERO;
   messageDataPoint.timestamp = timestamp;
   messageDataPoint.oracleMessage = messageID;
-  messageDataPoint.save();
 
-  // let ipfsDataArray = jsonData.value.toArray()
-  // for(let internalIndex = 0; internalIndex < ipfsDataArray.length; internalIndex++) {
-  //
-  // }
+  if (jsonIpfsData.value.kind == JSONValueKind.ARRAY) {
+    let ipfsDataArray = jsonIpfsData.value.toArray();
+    indexerDataPointCount = BigInt.fromI32(ipfsDataArray.length);
+
+    for (
+      let internalIndex = 0;
+      internalIndex < ipfsDataArray.length;
+      internalIndex++
+    ) {
+      let indexerDataPoint = new IndexerDataPoint(
+        [messageDataPoint.id, internalIndex.toString()].join("-")
+      );
+      indexerDataPoint.rawData = jsonObjectToString(
+        ipfsDataArray[internalIndex]
+      );
+      indexerDataPoint.messageDataPoint = messageDataPoint.id;
+      indexerDataPoint.save();
+    }
+  } else {
+    log.warning("IPFS data isn't an array for the MessageDataPoint", []);
+  }
+
+  messageDataPoint.indexerDataPointCount = indexerDataPointCount;
+  messageDataPoint.save();
 }
 
 /**
@@ -104,4 +127,65 @@ export function jsonToBigInt(val: JSONValue | null): BigInt {
     return val.toBigInt();
   }
   return BIGINT_ZERO;
+}
+
+/**
+ * Convert TypedMap to JSON string
+ */
+export function jsonObjectToString(val: JSONValue | null): String {
+  let str = "";
+  if (val != null && val.kind === JSONValueKind.OBJECT) {
+    let object = val.toObject();
+    str = "{";
+    for (let i = 0; i < object.entries.length; i++) {
+      str = str.concat(
+        '"' +
+          object.entries[i].key +
+          '":' +
+          jsonValueToString(object.entries[i].value)
+      );
+      if (i < object.entries.length - 1) {
+        str = str.concat(", ");
+      }
+    }
+    str = str.concat("}");
+  }
+  return str;
+}
+/*
+export enum JSONValueKind {
+  NULL = 0,
+  BOOL = 1,
+  NUMBER = 2,
+  STRING = 3,
+  ARRAY = 4,
+  OBJECT = 5,
+}
+*/
+export function jsonValueToString(val: JSONValue | null): String {
+  if (val == null) {
+    return "";
+  } else if (val.kind == JSONValueKind.NULL) {
+    return "null";
+  } else if (val.kind == JSONValueKind.BOOL) {
+    return val.toBool() ? "true" : "false";
+  } else if (val.kind == JSONValueKind.NUMBER) {
+    return BigDecimal.fromString(
+      changetype<string>(val.data as u32)
+    ).toString();
+  } else if (val.kind == JSONValueKind.STRING) {
+    return '"'.concat(val.toString()).concat('"');
+  } else if (val.kind == JSONValueKind.ARRAY) {
+    let arr = val.toArray();
+    let str = "[";
+    for (let i = 0; i < arr.length; i++) {
+      str += jsonValueToString(arr[i]);
+    }
+    str.concat("]");
+    return str;
+  } else if (val.kind == JSONValueKind.OBJECT) {
+    return jsonObjectToString(val);
+  } else {
+    return "";
+  }
 }
