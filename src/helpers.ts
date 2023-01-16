596ff971afb30f6101bfddd75a104c526f9d918c
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2023-01-16 20:06:38 -0300

    feat: add basic indexer and query daily bucketing

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,8 +5,22 @@ import {
   log,
   BigDecimal
 } from "@graphprotocol/graph-ts";
-import { Indexer, SubgraphDeployment } from "../generated/schema";
-import { BIGINT_ZERO } from "./constants";
+import {
+  Indexer,
+  SubgraphDeployment,
+  IndexerDataPoint,
+  QueryDataPoint,
+  IndexerDailyDataPoint,
+  QueryDailyDataPoint
+} from "../generated/schema";
+import {
+  BIGINT_ZERO,
+  SECONDS_PER_DAY,
+  LAUNCH_DAY,
+  BIGINT_ONE,
+  BIGDECIMAL_ZERO,
+  BIGDECIMAL_ONE
+} from "./constants";
 
 /**
  * Make sure the given JSONValue is a string and returns string it contains.
@@ -103,4 +117,176 @@ export function createDeployment(id: String): SubgraphDeployment {
     entity.save();
   }
   return entity as SubgraphDeployment;
+}
+
+export function compoundId(idA: string, idB: string): string {
+  return idA.concat("-").concat(idB);
+}
+
+export function getAndUpdateIndexerDailyData(
+  entity: IndexerDataPoint,
+  timestamp: BigInt
+): IndexerDailyDataPoint {
+  let dayNumber = timestamp.toI32() / SECONDS_PER_DAY - LAUNCH_DAY;
+  let id = compoundId(entity.id, BigInt.fromI32(dayNumber).toString());
+  let dailyData = IndexerDailyDataPoint.load(id);
+
+  if (dailyData == null) {
+    dailyData = new IndexerDailyDataPoint(id);
+
+    dailyData.dayStart = BigInt.fromI32(
+      (timestamp.toI32() / SECONDS_PER_DAY) * SECONDS_PER_DAY
+    );
+    dailyData.dayEnd = dailyData.dayStart + BigInt.fromI32(SECONDS_PER_DAY);
+    dailyData.dayNumber = dayNumber;
+    dailyData.indexer = entity.indexer;
+    dailyData.dataPointCount = BIGINT_ZERO;
+    dailyData.indexer_url = entity.indexer_url;
+    dailyData.indexer_wallet = entity.indexer_wallet;
+    dailyData.query_count = BIGDECIMAL_ZERO;
+    dailyData.start_epoch = entity.start_epoch;
+  }
+
+  let prevWeight = dailyData.query_count;
+
+  dailyData.dataPointCount = dailyData.dataPointCount.plus(BIGINT_ONE);
+  dailyData.query_count = dailyData.query_count + entity.query_count;
+
+  dailyData.avg_indexer_blocks_behind = bdWeightedAverage(
+    [
+      [dailyData.avg_indexer_blocks_behind, prevWeight],
+      [entity.avg_indexer_blocks_behind, entity.query_count]
+    ],
+    dailyData.query_count
+  );
+  dailyData.avg_indexer_latency_ms = bdWeightedAverage(
+    [
+      [dailyData.avg_indexer_latency_ms, prevWeight],
+      [entity.avg_indexer_latency_ms, entity.query_count]
+    ],
+    dailyData.query_count
+  );
+  dailyData.avg_query_fee = bdWeightedAverage(
+    [
+      [dailyData.avg_query_fee, prevWeight],
+      [entity.avg_query_fee, entity.query_count]
+    ],
+    dailyData.query_count
+  );
+  dailyData.end_epoch = entity.end_epoch;
+  dailyData.max_indexer_blocks_behind = maxBD(
+    dailyData.max_indexer_blocks_behind,
+    entity.max_indexer_blocks_behind
+  );
+  dailyData.max_indexer_latency_ms = maxBD(
+    dailyData.max_indexer_latency_ms,
+    entity.max_indexer_latency_ms
+  );
+  dailyData.max_query_fee = maxBD(
+    dailyData.max_query_fee,
+    entity.max_query_fee
+  );
+  dailyData.num_indexer_200_responses =
+    dailyData.num_indexer_200_responses + entity.num_indexer_200_responses;
+  dailyData.proportion_indexer_200_responses = bdWeightedAverage(
+    [
+      [dailyData.proportion_indexer_200_responses, prevWeight],
+      [entity.proportion_indexer_200_responses, entity.query_count]
+    ],
+    dailyData.query_count
+  );
+  dailyData.total_query_fees =
+    dailyData.total_query_fees + entity.total_query_fees;
+
+  dailyData.save();
+
+  return dailyData as IndexerDailyDataPoint;
+}
+
+export function getAndUpdateQueryDailyData(
+  entity: QueryDataPoint,
+  timestamp: BigInt
+): QueryDailyDataPoint {
+  let dayNumber = timestamp.toI32() / SECONDS_PER_DAY - LAUNCH_DAY;
+  let id = compoundId(entity.id, BigInt.fromI32(dayNumber).toString());
+  let dailyData = QueryDailyDataPoint.load(id);
+
+  if (dailyData == null) {
+    dailyData = new QueryDailyDataPoint(id);
+
+    dailyData.dayStart = BigInt.fromI32(
+      (timestamp.toI32() / SECONDS_PER_DAY) * SECONDS_PER_DAY
+    );
+    dailyData.dayEnd = dailyData.dayStart + BigInt.fromI32(SECONDS_PER_DAY);
+    dailyData.dayNumber = dayNumber;
+    dailyData.subgraphDeployment = entity.subgraphDeployment;
+    dailyData.dataPointCount = BIGINT_ZERO;
+    dailyData.query_count = BIGDECIMAL_ZERO;
+    dailyData.start_epoch = entity.start_epoch;
+  }
+
+  let prevWeight = dailyData.query_count;
+
+  dailyData.dataPointCount = dailyData.dataPointCount.plus(BIGINT_ONE);
+  dailyData.query_count = dailyData.query_count + entity.query_count;
+
+  dailyData.avg_gateway_latency_ms = bdWeightedAverage(
+    [
+      [dailyData.avg_gateway_latency_ms, prevWeight],
+      [entity.avg_gateway_latency_ms, entity.query_count]
+    ],
+    dailyData.query_count
+  );
+  dailyData.avg_query_fee = bdWeightedAverage(
+    [
+      [dailyData.avg_query_fee, prevWeight],
+      [entity.avg_query_fee, entity.query_count]
+    ],
+    dailyData.query_count
+  );
+  dailyData.end_epoch = entity.end_epoch;
+  dailyData.gateway_query_success_rate = bdWeightedAverage(
+    [
+      [dailyData.gateway_query_success_rate, prevWeight],
+      [entity.gateway_query_success_rate, entity.query_count]
+    ],
+    dailyData.query_count
+  );
+  dailyData.max_gateway_latency_ms = maxBD(
+    dailyData.max_gateway_latency_ms,
+    entity.max_gateway_latency_ms
+  );
+  dailyData.max_query_fee = maxBD(
+    dailyData.max_query_fee,
+    entity.max_query_fee
+  );
+  dailyData.most_recent_query_ts = entity.most_recent_query_ts;
+  dailyData.total_query_fees =
+    dailyData.total_query_fees + entity.total_query_fees;
+  dailyData.user_attributed_error_rate = bdWeightedAverage(
+    [
+      [dailyData.user_attributed_error_rate, prevWeight],
+      [entity.user_attributed_error_rate, entity.query_count]
+    ],
+    dailyData.query_count
+  );
+
+  dailyData.save();
+
+  return dailyData as QueryDailyDataPoint;
+}
+
+export function bdWeightedAverage(
+  arr: Array<Array<BigDecimal>>,
+  totalWeight: BigDecimal
+): BigDecimal {
+  let result = BigDecimal.fromString("0");
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i][0] * (arr[i][1] / totalWeight);
+  }
+  return result;
+}
+
+export function maxBD(a: BigDecimal, b: BigDecimal): BigDecimal {
+  return a > b ? a : b;
 }
